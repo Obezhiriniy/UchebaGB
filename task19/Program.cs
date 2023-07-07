99dea6a883f5c6586a651960e15418cc6ffcd476
n.pomoynitskiy <n.pomoynitskiy@ll-games.com>
2023-07-08 01:02:44 +0300

    task19

--- task19/Program.cs
+++ task19/Program.cs
@@ -1,3 +1,21 @@
 ﻿// Программа которая принимает пятизначное число и выесняет, является ли оно палиндромом
 
+Console.WriteLine("Введите пятизначное число");
+int number = Convert.ToInt32(Console.ReadLine());
 
+if (number > 9999 && number < 100000)
+{
+bool check = true;
+
+if (check ? firstNumber == fifthNumber && secondNumber == fifthNumber);
+
+Console.WriteLine( true ? "Число палиндром");  
+
+Console.WriteLine(false ? "Число не палиндром");  
+
+}
+
+else 
+{
+    Console.WriteLine("Ошибка, введите, пятизначное число"); 
+}
